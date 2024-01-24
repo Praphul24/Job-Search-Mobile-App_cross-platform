60c9a6753bb9c13b6f7982b92fd0e4e0fcfc61e0
Praphul Yadav <praphulyadav150@gmail.com>
2024-01-24 21:14:34 +0530

    Updated to SDK 49

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -21,8 +21,8 @@ const Welcome = ({ searchTerm, setSearchTerm, handleClick }) => {
   return (
     <View>
       <View style={styles.container}>
-        <Text style={styles.userName}>Hello Adrian</Text>
-        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+        <Text style={styles.userName}>Hello Praphul</Text>
+        <Text style={styles.welcomeMessage}>Look for a perfect job</Text>
       </View>
 
       <View style={styles.searchContainer}>
